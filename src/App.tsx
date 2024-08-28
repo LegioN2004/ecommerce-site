42b7026250c7871daa4eaf6e70e4f243e47d24f4
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2024-08-28 19:43:08 +0530

    initial commit to the ecom project

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>heloooooooo</h1>
+      <p class="text-3xl text-blue-500">Lorem, ipsum.</p>
+    </>
+  );
+}
+
+export default App;
